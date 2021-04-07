09429f9506f6cac73b7a02bf48ccf5f980df1da9
sabiokamille <sabikamille@gmail.com>
2021-04-07 17:23:55 -0400

    first step

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
+import picture from './images/picture.png'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1> Header 1 </h1>
-        <h3> Header 2 </h3>
+        <h1> Happy Anniversary! </h1>
+        <h3 class="obama"> Header 2 </h3>
+        <h1 id="rob"> New Header </h1>
+        <img src={picture} alt="picture" class="App-logo"/>
       </div>
     )
   }
